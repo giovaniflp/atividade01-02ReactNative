d04bf28adf94220df89008b7a7ddc05a1e593e67
aluno <aluno@ifpe>
2024-03-14 18:57:26 -0300

    firebase config e login

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBPtbT1pmonW84fioMYrrt1C7MsU3ZLPs",
+  authDomain: "atividade-nilson.firebaseapp.com",
+  projectId: "atividade-nilson",
+  storageBucket: "atividade-nilson.appspot.com",
+  messagingSenderId: "216015940541",
+  appId: "1:216015940541:web:44e0b154e60a3c6d08c0d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
